3e6705cb9d71f4d685a213eb1894ed2d23657ca9
Nobutaka <decepticoooon@gmail.com>
2019-02-04 14:36:34 +0000

    もういっかい

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,5 +6,18 @@ class Category < ActiveRecord::Base
   validates :name,  presence: true
   
   
+  def post_tools_count()
+    
+    posts_num = 0
+    
+    self.tools.each do |t|
+      posts_num += t.posttools.count
+    end
+      
+   return posts_num
+      
+  end 
+  
+  
   
 end
