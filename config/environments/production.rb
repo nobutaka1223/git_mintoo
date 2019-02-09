11e225a17df46f7864614bb46f1f51479170d2cc
Nobutaka <decepticoooon@gmail.com>
2019-02-09 07:18:01 +0000

    mailerの設定4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   
-  host = 'mintoo.herokuapp.com'
+  host = 'mintoo'
   config.action_mailer.default_url_options = { host: host }
   
    # Don't care if the mailer can't send.
